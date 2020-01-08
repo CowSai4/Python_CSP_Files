b3316faeebf7f0e42a4fe82494310fcb5b534a82
Somsai Chittakhone <chittaks02@Student.psdr3.org>
2020-01-08 12:35:39 -0600

    Do more

--- MyPLTW214/a214_simple_window1_sc.py
+++ MyPLTW214/a214_simple_window1_sc.py
@@ -2,10 +2,20 @@
 #   a113_TR_simple_window1.py
 #   Example soulution: Change its size to 200 by 100 pixels.
 ##############################################################################
-import tkinter as Authorization
+import tkinter as tk
 
 # main window
-root = Authorization.Tk()
+root = tk.Tk()
 root.wm_geometry("200x100")
+root.wm_title('Authorization')
+
+# create empty frame
+frame_login = tk.Frame(root)
+frame_login.grid()
+
+lbl_username = tk.Label(frame_login, text='Username:')
+lbl_username.pack()
+
+tk.Label(frame_login,text="Password:",font="Courier")
 
 root.mainloop()
