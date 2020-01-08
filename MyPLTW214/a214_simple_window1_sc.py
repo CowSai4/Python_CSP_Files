77dd8cebea6c55e89c82b69de2228a81512b4cb4
Somsai Chittakhone <chittaks02@Student.psdr3.org>
2020-01-08 12:15:44 -0600

    200x100

--- MyPLTW214/a214_simple_window1_sc.py
+++ MyPLTW214/a214_simple_window1_sc.py
@@ -2,10 +2,10 @@
 #   a113_TR_simple_window1.py
 #   Example soulution: Change its size to 200 by 100 pixels.
 ##############################################################################
-import tkinter as tk
+import tkinter as Authorization
 
 # main window
-root = tk.Tk()
-root.wm_geometry("200x200")
+root = Authorization.Tk()
+root.wm_geometry("200x100")
 
 root.mainloop()
